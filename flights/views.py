fa67cc2832e145ac5e3b0ddcbf17129356ec3151
George Papagapitos <george.g.papagapitos@gmail.com>
2021-05-07 11:59:22 -0500

    added login / logout functionality

--- flights/views.py
+++ flights/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import Flight
+from .models import Flight, Passenger
 
 # Create your views here.
 def index(request):
@@ -14,7 +14,8 @@ def flight(request, flight_id):
   flight = Flight.objects.get(pk=flight_id)
   return render(request, "flights/flight.html", {
     "flight": flight,
-    "passengers": flight.passengers.all()
+    "passengers": flight.passengers.all(),
+    "non_passengers": Passenger.objects.exclude(flights=flight).all()
   })
 
 def book(request, flight_id):
@@ -22,4 +23,4 @@ def book(request, flight_id):
     flight = Flight.objects.get(pk=flight_id)
     passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
     passenger.flights.add(flight)
-    return HttpResponseRedirect(reverse("flight", args=(flight.id)))
+    return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
